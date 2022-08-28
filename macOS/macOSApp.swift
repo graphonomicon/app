9f25d63e7959c7bee7037814598073a9ba68e904
vaux <zero.griffin@gmail.com>
2022-08-28 13:47:55 +0200

    Configure project

---
+++ macOS/macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSApp.swift
+//  macOS
+//
+//  Created by vaux on 28.08.22.
+//
+
+import SwiftUI
+
+@main
+struct macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
